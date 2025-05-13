3f05b921aed531facf3195e60b8700de4a3eaaf9
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-05-13 10:08:30 -0400

    Sorted character count. Changed output formatting.

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from stats import get_word_count
+
+from stats import get_word_count, get_char_count, sort_char_count
 
 def get_book_text(filepath):
     with open(filepath) as file:
         return file.read()
     
+def print_word_count(count):
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
 
+def print_char_count(char_count):
+    print("--------- Character Count -------")
+    for entry in char_count:
+        if entry["char"].isalpha():
+            print(f"'{entry["char"]}': {entry["num"]}")
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+
     book_text = get_book_text("books/frankenstein.txt")
-    count = get_word_count(book_text)
-    print(f"{count} words found in the document")
+    wordcount = get_word_count(book_text)
+    
+    print_word_count(wordcount)
+
+    char_count = get_char_count(book_text)
+    char_count_sorted = sort_char_count(char_count)
+
+    print_char_count(char_count_sorted)
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    textlower = text.lower()
+    chardict = {}
+    for char in textlower:
+        if char in chardict:
+            chardict[char] += 1
+        else:
+            chardict[char] = 1
+    return chardict
+    
+
+def sort_on_num(dict):
+    return dict["num"]
+
+def sort_char_count(char_count):
+    charlist = []
+    for key in char_count:
+        charlist.append({"char": key, "num": char_count[key]})
+    charlist.sort(reverse=True, key=sort_on_num)
+    return charlist
